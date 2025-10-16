49a165d836ff9f31fb6c8f5b9229243adb072446
C-Neoptolemus <cassady.fattor@gmail.com>
2025-10-15 19:09:58 -0600

    initial commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Opmodes/BaseAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Opmodes/BaseAuto.java
@@ -2,12 +2,14 @@ package org.firstinspires.ftc.teamcode.Opmodes;
 
 import com.acmerobotics.dashboard.config.Config;
 
+import org.firstinspires.ftc.teamcode.Firmware.Robot;
 import org.firstinspires.ftc.teamcode.Robot.Robot;
 
 // This class will have the autonomous functions applicable to every auto. All autos extend BaseAuto.
 @Config
-abstract public class BaseAuto extends org.firstinspires.ftc.teamcode.OpModes.GeneralOpMode {
+abstract public class BaseAuto extends org.firstinspires.ftc.teamcode.Opmodes.GeneralOpMode {
     public void setAutoSpeedSlow(){
+        Robot robot = null;
         robot.setRobotSpeed(Robot.Speed.SLOW);
     }
     public void setAutoSpeedFast(){
