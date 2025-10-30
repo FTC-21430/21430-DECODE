3f51e4a9dbca846e7f72d80f53d0384c2c66ba89
C-Neoptolemus <cassady.fattor@gmail.com>
2025-10-29 19:46:31 -0600

    Update CompetitionTeleop.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Opmodes/CompetitionTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Opmodes/CompetitionTeleop.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 package org.firstinspires.ftc.teamcode.Opmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -30,3 +31,45 @@ public class CompetitionTeleop extends BaseTeleOp {
         }
     }
 }
+=======
+package org.firstinspires.ftc.teamcode.OpModes;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+    @TeleOp
+    public class CompetitionTeleop extends BaseTeleOp {
+
+        // Teleop Logic
+
+        // the angle the robot should be facing
+        double targetAngle = 0;
+
+        // stores if we are currently inputting a turn on controller 1
+        boolean gp1xJoy = false;
+        boolean gp2tri = false;
+        
+        // The main code that runs during init
+        @Override
+        public void runOpMode() throws InterruptedException {
+
+            // initializes the robot without resetting the odometry
+            initialize(false, false);
+
+
+            waitForStart();
+
+            while(opModeIsActive()) {
+
+                // get and update functions
+                robot.updateLoopTime();
+                robot.odometry.updateOdometry();
+
+                // resets Field Centric Driving
+                if (gamepad1.share) {
+                    robot.IMUReset();
+                }
+                        robot.setTurnPIntake(false);
+                    }
+
+                        }
+                    }
+>>>>>>> Stashed changes
