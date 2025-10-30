113a7c5be477e1a7fab9c34911da509d9bcc504e
C-Neoptolemus <cassady.fattor@gmail.com>
2025-10-29 19:45:48 -0600

    TeleopWork

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Opmodes/CompetitionTeleop.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode.Opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class CompetitionTeleop extends BaseTeleOp {
+
+    // The main code that runs during init
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        // initializes the robot without resetting the odometry
+        initialize(false, false);
+
+        waitForStart();
+
+        while(opModeIsActive()) {
+
+            // get and update functions
+            robot.updateLoopTime();
+            robot.odometry.updateOdometry();
+
+            // resets Field Centric Driving
+            if (gamepad1.share) {
+                robot.IMUReset();
+            }
+
+            robot.driveTrain.setDrivePower(-gamepad1.left_stick_y, gamepad1.left_stick_x, robot.anglePID.getPower(), robot.odometry.getRobotAngle());
+            robot.updateRobot(false, false, false);
+        }
+    }
+}
