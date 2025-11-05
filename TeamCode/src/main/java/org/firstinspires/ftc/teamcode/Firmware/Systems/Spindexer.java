41b5d56920cca654f68d243fcbc136f02b2955e3
FriendlyGuardian <tobin.rumsey@gmail.com>
2025-11-04 22:51:01 -0700

    Spindexer progress

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Firmware/Systems/Spindexer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Firmware/Systems/Spindexer.java
@@ -1,4 +1,25 @@
 package org.firstinspires.ftc.teamcode.Firmware.Systems;
 
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import org.firstinspires.ftc.teamcode.Firmware.Systems.SpindexerColorSensor.COLORS;
+
 public class Spindexer {
+
+    private final SpindexerServoFirmware servo;
+
+    public Spindexer(HardwareMap hardwareMap){
+//        TODO: ensure that the slot values and the config address name are correct
+        servo = new SpindexerServoFirmware(hardwareMap,false,0,90,180,"intake");
+    }
+
+    public void prepColor(COLORS color){
+        if (color == COLORS.NONE) {
+            return;
+        }
+        // figure out what index the correct color is in
+        // move that index to launch pos
+    }
+    public void eject(){
+        if ()
+    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Firmware/Systems/SpindexerServoFirmware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Firmware/Systems/SpindexerServoFirmware.java
@@ -3,47 +3,94 @@ package org.firstinspires.ftc.teamcode.Firmware.Systems;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range;
 
-import org.firstinspires.ftc.teamcode.Resources.ServoPlus;
-
+/**
+ * Current class assumptions:
+ *The servo has a "dead spot"
+ */
 public class SpindexerServoFirmware {
     private final Servo spindexerServo;
-    private final double[] positions;
+    // the degree values for each slot
+    private final double[] slots;
+    private final double direction;
+    private double encoderPosition; // in degrees
 
-    private final double passPWM;
-    private int encoderPosition;
+    // The servo degree that is the current target
+    private double targetPosition = 0;
 
+    // how many degrees of tolerance there will be for the isAtTarget() Function to return true
     private int positionTolerance = 4;
 
+    // Warp speed exit tolerance - The servo will always spin in one direction at full continuous speed until
+    // we get close enough to the target position that the servo will be in range (not in the gap area outside of its it's range)
+    // At this point, we will directly address the servo PWM to the position that we are trying to stop at.
+    // This is the solution to being able to always turn one way and also use the limited range servo features of this servo.
+
+    private double warpSpeedExitTolerance = 15;
     private DcMotor spindexerEncoderMotorInstance;
-    public SpindexerServoFirmware(HardwareMap hardwareMap, boolean spinClockwise, double position1, double position2, double position3, String encoderConfigAddress){
-        this.positions = new double[] {position1,position2,position3};
+
+    private double controlledRangeMinDegree = 10;
+    private double controlledRangeMaxDegree = 250;
+    public SpindexerServoFirmware(HardwareMap hardwareMap, boolean spinClockwise, double slot1, double slot2, double slot3, String encoderConfigAddress){
+        this.slots = new double[] {slot1,slot2,slot3};
         spindexerServo = hardwareMap.get(Servo.class, "spindexer servo");
         spindexerEncoderMotorInstance = hardwareMap.get(DcMotor.class, encoderConfigAddress);
+
         if (spinClockwise){
-            passPWM = -1;
+            direction = -1;
         } else{
-            passPWM = 1;
+            direction = 1;
         }
     }
 
-    public void setSpindexerPosition(int position){
+    public void update(){
+        encoderPosition = getEncoderPosition();
+        if (Math.abs(encoderPosition - targetPosition) <= warpSpeedExitTolerance){
+            spindexerServo.setPosition(degreesToServoPWM(targetPosition));
+        } else {
+            spindexerServo.setPosition(direction);
+        }
+    }
 
+    public void setSpindexerPosition(double position){
+        if (position > 360){
+            position = position % 360;
+        }
+        position = clipPositionToRange(position);
+        targetPosition = position;
+        update();
+    }
+
+    // valid for slots 1-3
+    public void setSpindexerSlot(int slot){
+        if (slot > 0 && slot < 4)
+        setSpindexerPosition(slots[slot-1]);
     }
 
     public void setPositionTolerance(int toleranceTicks){
         positionTolerance = toleranceTicks;
     }
-    public boolean isAtTargetSlot(){
-        return false; //TODO: 
+    public boolean isAtTarget(){
+        return Math.abs(getEncoderPosition()-targetPosition) < positionTolerance;
     }
 
+    public double getTargetPosition() {
+        return targetPosition;
+    }
+
+    public double getEncoderPosition(){
+        return this.encoderTicksToDegrees(spindexerEncoderMotorInstance.getCurrentPosition());
+    }
     private double encoderTicksToDegrees(int ticks){
-        return (double)ticks; // TODO: insert regression function here.
+        return ticks; // TODO: insert regression function here.
     }
 
     private double degreesToServoPWM(double degrees){
         return 0.0; //TODO: make this regression function
     }
 
+    private double clipPositionToRange( double position){
+        return Range.clip(position, controlledRangeMinDegree, controlledRangeMaxDegree);
+    }
 }
