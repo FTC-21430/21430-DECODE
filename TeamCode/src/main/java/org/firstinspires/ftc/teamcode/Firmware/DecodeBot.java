57cacac95beba0b6c6b2c9f4eb9f939d7ff79e6b
FriendlyGuardian <tobin.rumsey@gmail.com>
2025-11-08 19:24:43 -0700

    Integrating auto aim

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Firmware/DecodeBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Firmware/DecodeBot.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.Firmware;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
@@ -8,24 +9,34 @@ import org.firstinspires.ftc.teamcode.Firmware.Systems.GobildaPinpointModuleFirm
 import org.firstinspires.ftc.teamcode.Firmware.Systems.Launcher;
 import org.firstinspires.ftc.teamcode.Firmware.Systems.MecanumDriveTrain;
 import org.firstinspires.ftc.teamcode.Resources.PathFollowing;
+import org.firstinspires.ftc.teamcode.Resources.TrajectoryKinematics;
 
+@Config
 public class DecodeBot extends Robot{
 
+//    TODO this value must be found through testing as be do not know the constant of kinetic friction between artifacts and the flywheel.
+    // the conversion ratio of the speed of the ball's movement to the robots flywheel speed
+    public static double velocityMetersToDegrees = 0.03;
     public Launcher launcher = null;
+    public TrajectoryKinematics trajectoryKinematics;
+
+    public String alliance = "red";
     //The PID values are a public because we need to tune it later and public makes it easier to do that
     public static final double P_CONSTANT = 0.15;
     public static final double I_CONSTANT = 0.1;
     public static final double D_CONSTANT = 0.02;
     @Override
-    public void init(HardwareMap hardwareMap, Telemetry telemetry, double robotX, double robotY, double robotAngle, LinearOpMode opMode, boolean reset, boolean isAuto){
+    public void init(HardwareMap hardwareMap, Telemetry telemetry, double robotX, double robotY, double robotAngle, LinearOpMode opMode, boolean reset, boolean isAuto, String alliance){
         pathFollowing = new PathFollowing(P_CONSTANT, P_CONSTANT, I_CONSTANT, I_CONSTANT, D_CONSTANT, D_CONSTANT, runtime);
         this.opMode = opMode;
 
         this.telemetry = telemetry;
 
+        this.alliance = alliance;
         // TODO: change the pod offset values to what they are on the competition robot, currently tuned for software testing bot
         odometry = new GobildaPinpointModuleFirmware(hardwareMap, 7.2,-18,reset);
 
+        trajectoryKinematics = new TrajectoryKinematics(40,30);
         bulkSensorBucket = new BulkSensorBucket(hardwareMap);
 
         driveTrain = new MecanumDriveTrain(hardwareMap, telemetry);
@@ -33,5 +44,12 @@ public class DecodeBot extends Robot{
 
         bulkSensorBucket.clearCache();
 
+    }
+
+    public void aimBasedOnTags(){
+        // TODO: replace the 0.0 to the output of april tags
+        trajectoryKinematics.calculateTrajectory(0.0);
+        launcher.setSpeed(trajectoryKinematics.getLaunchMagnitude() * velocityMetersToDegrees);
+        
     }
 }
