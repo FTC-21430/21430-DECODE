87f5e7575eb8ac4656a26c2ca247c3d1f8d73b15
FriendlyGuardian <tobin.rumsey@gmail.com>
2025-11-12 18:50:45 -0700

    needed import statements added

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Firmware/Systems/AprilTag.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Firmware/Systems/AprilTag.java
@@ -11,6 +11,8 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
