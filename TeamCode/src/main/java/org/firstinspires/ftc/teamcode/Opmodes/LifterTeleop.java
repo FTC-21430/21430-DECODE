0e0e79e26e95aa9ea5d9c5fb7cccdca194c48906
C-Neoptolemus <cassady.fattor@gmail.com>
2025-12-13 13:00:39 -0700

    Create Lifter teleop!

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Opmodes/LifterTeleop.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode.Opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import org.firstinspires.ftc.teamcode.Firmware.Systems.Lifter;
+
+public class LifterTeleop extends LinearOpMode {
+private Lifter lift = null;
+    @Override
+    public void runOpMode() throws InterruptedException {
+        lift = new Lifter(hardwareMap, telemetry);
+        waitForStart();
+        while (opModeIsActive()) {
+            if (gamepad1.squareWasPressed()) {
+                //lift
+                lift.lift();
+            }
+            if (gamepad1.crossWasPressed()) {
+                //lower
+                lift.retract();
+            }
+            if (gamepad1.rightBumperWasPressed()) {
+                //defence
+                lift.defence();
+            }
+            if (gamepad1.triangleWasPressed()) {
+                //open latches
+                lift.unlockLatches();
+            }
+            if (gamepad1.circleWasPressed()) {
+                //closes latches
+                lift.lockLatches();
+            }
+            lift.update();
+        }
+    }
+}
