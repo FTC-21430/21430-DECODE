40f838ea73dfde2c73c37fdceed5208319b9e7d9
C-Neoptolemus <cassady.fattor@gmail.com>
2025-12-30 12:02:19 -0700

    Refactor BlueBeaver autonomous routine and add motif detection
    Refactored the BlueBeaver autonomous routine to use the new sortedLaunch method with a finalLaunch parameter, integrated motif detection during movement, and updated robot movement coordinates for improved pathing. Added initialization for spindexer color indexing and index offset, and replaced some hardcoded moves with detectMotifWhileMoveTo and chillAndDetect calls for better object detection and intake handling.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Opmodes/Autonomous/BlueBeaver.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Opmodes/Autonomous/BlueBeaver.java
@@ -1,84 +1,97 @@
 package org.firstinspires.ftc.teamcode.Opmodes.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.Firmware.OperatorStateMachine;
+import org.firstinspires.ftc.teamcode.Firmware.Systems.Spindexer;
+import org.firstinspires.ftc.teamcode.Firmware.Systems.SpindexerColorSensor;
 import org.firstinspires.ftc.teamcode.Opmodes.BaseAuto;
 
 @Autonomous
 public class BlueBeaver extends BaseAuto {
 
-    private void sortedLaunch(){
+    private void sortedLaunch(boolean finalLaunch){
+//        robot.autoMoveTo(0, 40, 135, 2);
         //The robot moves to the launch zone and it launches the three balls
-        robot.chill(true,3);
-        robot.autoMoveTo(-8, -10, 225, 2);
-        robot.chill(true,5);
-//        autonomousLaunching(motifId);
+        if (!finalLaunch) {
+            robot.autoMoveTo(-23.5, -24, 135, 4);
+        }else{
+            robot.autoMoveTo(-40, -18, 135, 4);
+        }
+        robot.chill(true,0.1);
+        autonomousLaunching(motifId);
     }
 
     @Override
     public void runOpMode() throws InterruptedException {
         initialize(true, true);
         robot.odometry.recalibrateIMU();
+        robot.spindexer.setColorIndexing(SpindexerColorSensor.COLORS.GREEN, SpindexerColorSensor.COLORS.PURPLE, SpindexerColorSensor.COLORS.PURPLE);
 
         waitForStart();
-        //TODO; tune starting positon for actual robot
         //This is the starting location of the robot
-        robot.odometry.overridePosition(-50,-50.8,230.7);
-
+        robot.odometry.overridePosition(-49.82,-54.5782,234.92);
+        robot.spindexer.setIndexOffset(Spindexer.INDEX_TYPE.NONE);
         
 
         //This is the position that the robot moves to to shoot the first three balls
-        robot.autoMoveTo(-15,-20,225,2);
-
-        //TODO: Figure out the shooting code with actual robot
-//        robot.launchFrom("mid");
-
-
+        motifId = 0;
+        sortedLaunch(false);
+        detectMotifWhileMoveTo(-39,-25,150,2);
+        robot.operatorStateMachine.moveToState(OperatorStateMachine.State.INTAKE);
+        chillAndDetect(true,0.2);
 //        //The robot moves to the place to intake the balls
-//        robot.operatorStateMachine.moveToState(OperatorStateMachine.State.INTAKE);
-        detectMotifWhileMoveTo(-23,-44,160,2);
+        detectMotifWhileMoveTo(-32,-46,180,2);
+        chillAndDetect(true,0.1);
 
-        detectMotifWhileMoveTo(-5,-46,180,2);
+        detectMotifWhileMoveTo(-27,-46,180,3);
+
+
+        detectMotifWhileMoveTo(-6,-46,180,4);
 
 //
 //        //The robot bumps the gate
-        detectMotifWhileMoveTo(-2,-51,180,2);
+        detectMotifWhileMoveTo(-6,-50,180,2);
+        chillAndDetect(true,1);
 
-        detectMotifWhileMoveTo(-2,-55,180,2);
+        detectMotifWhileMoveTo(-6,-53.3,180,3);
+        chillAndDetect(true,1);
+
 //
-        sortedLaunch();
+        sortedLaunch(false);
 //
 //        // sorted cycle 1
 //
 //        //The robot moves to the place to intake the balls
-//        robot.operatorStateMachine.moveToState(OperatorStateMachine.State.INTAKE);
-        robot.autoMoveTo(0,-44,180,2);
+        robot.operatorStateMachine.moveToState(OperatorStateMachine.State.INTAKE);
+        detectMotifWhileMoveTo(0,-44.0,175,3);
 
-        robot.autoMoveTo(18.5,-46,180,2);
+        detectMotifWhileMoveTo(15.5,-44.5,180,4);
 
 
-        robot.chill(true, 0.5);
+        robot.chill(true, 0.8);
 //
-        sortedLaunch();
+        sortedLaunch(false);
 //
 //        // sorted cycle 2
 //
 //        //The robot moves to the place to intake the balls
-//        robot.operatorStateMachine.moveToState(OperatorStateMachine.State.INTAKE);
-        robot.autoMoveTo(23.5,-44,180,2);
+        robot.operatorStateMachine.moveToState(OperatorStateMachine.State.INTAKE);
+        detectMotifWhileMoveTo(18.5,-41,180,5);
 
 
-        robot.autoMoveTo(42,-46,180,2);
-        robot.chill(true, 0.5);
+        detectMotifWhileMoveTo(31,-41,180,4);
+        robot.chill(true, 0.8);
 
 
 //
-        sortedLaunch();
+        sortedLaunch(true);
 //
 //        // sorted cycle 3
 //
 ////      park off of launch line and close to the gate to clear the classifier at teleop start
-        robot.autoMoveTo(-2,-51,180,2);
-        robot.chill(true,2);
+        robot.autoMoveTo(-2,-46,180,2);
+        robot.chill(true,0.2);
 
 
    }
-    }
+}
