1b5d0cba2d98820f35f4513181f8e5937763d7a4
C-Neoptolemus <cassady.fattor@gmail.com>
2026-01-24 13:10:53 -0700

    Add RedDolphin and RedElephant autonomous modes
    Introduces two new autonomous opmodes, RedDolphin and RedElephant, for the FTC robot. RedDolphin is designed for the far zone start, while RedElephant is for the close zone, each with specific initialization and movement routines tailored to their starting positions.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Opmodes/Autonomous/RedDolphin.java
@@ -0,0 +1,39 @@
+package org.firstinspires.ftc.teamcode.Opmodes.Autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import org.firstinspires.ftc.teamcode.Firmware.OperatorStateMachine;
+import org.firstinspires.ftc.teamcode.Firmware.Systems.Spindexer;
+import org.firstinspires.ftc.teamcode.Firmware.Systems.SpindexerColorSensor;
+import org.firstinspires.ftc.teamcode.Opmodes.BaseAuto;
+
+// This Autonomous is for when the robot starts in the far zone
+@Autonomous
+public class RedDolphin extends BaseAuto {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        initialize(true, true);
+        robot.setAlliance("red");
+        robot.odometry.recalibrateIMU();
+        robot.spindexer.setColorIndexing(SpindexerColorSensor.COLORS.GREEN, SpindexerColorSensor.COLORS.PURPLE, SpindexerColorSensor.COLORS.PURPLE);
+
+        while (opModeInInit()){
+            int tempID = robot.aprilTags.getMotifID();
+            if (tempID != 0) motifId = tempID;
+            telemetry.addData("CurrentMotif", motifId);
+            telemetry.update();
+        }
+        robot.setAlliance("red");
+        //This is the starting location of the robot
+        //robot.odometry.overridePosition(,,,);
+        robot.spindexer.setIndexOffset(Spindexer.INDEX_TYPE.NONE);
+        robot.chill(false,0.2);
+
+        // launch preloads
+        robot.aimAtGoal();
+        autonomousLaunching(motifId);
+
+        //move off the line
+        //robot.autoMoveTo(,,,);
+        //end auto
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Opmodes/Autonomous/RedElephant.java
@@ -0,0 +1,72 @@
+package org.firstinspires.ftc.teamcode.Opmodes.Autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.Firmware.OperatorStateMachine;
+import org.firstinspires.ftc.teamcode.Firmware.Systems.Spindexer;
+import org.firstinspires.ftc.teamcode.Firmware.Systems.SpindexerColorSensor;
+import org.firstinspires.ftc.teamcode.Opmodes.BaseAuto;
+
+// This Autonomous is for when the robot starts in the close zone
+@Autonomous
+public class RedElephant extends BaseAuto {
+    private void sortedLaunch(boolean finalLaunch, boolean firstLaunch){
+        if (!firstLaunch) {
+            robot.autoMoveTo(-15.5, 30, 195, 25);
+        }
+//        robot.autoMoveTo(0, 40, 135, 2);
+        //The robot moves to the launch zone and it launches the three balls
+        if (!finalLaunch) {
+            if (firstLaunch){
+                robot.autoMoveTo(-15.5, 19, 132, 8);
+            }else{
+                robot.autoMoveTo(-15.5, 19, 134, 8);
+            }
+
+        }else{
+            robot.autoMoveTo(-35, 18, 116, 4);
+        }
+        if (!firstLaunch) {
+            robot.chill(true, 0.8);
+        }else{
+            robot.chill(true,0.3);
+        }
+        robot.aimAtGoal();
+        robot.setLauncherBasedOnTags();
+
+        robot.chill(true,0.3);
+        robot.aimAtGoal();
+        autonomousLaunching(motifId);
+//
+    }
+    @Override
+    public void runOpMode() throws InterruptedException {
+        initialize(true, true);
+        robot.setAlliance("red");
+        robot.odometry.recalibrateIMU();
+        robot.spindexer.setColorIndexing(SpindexerColorSensor.COLORS.GREEN, SpindexerColorSensor.COLORS.PURPLE, SpindexerColorSensor.COLORS.PURPLE);
+
+        waitForStart();
+        //This is the starting location of the robot
+        robot.odometry.overridePosition(-49.82,54.5782,125.08);
+        robot.spindexer.setIndexOffset(Spindexer.INDEX_TYPE.NONE);
+        robot.chill(false,0.2);
+        //This is the position that the robot moves to to shoot the first three balls
+        motifId = 0;
+        robot.aimAtGoal();
+        robot.chill(true,0.2);
+        sortedLaunch(false, true);
+        detectMotifWhileMoveTo(-39,40,210,10);
+        robot.operatorStateMachine.moveToState(OperatorStateMachine.State.INTAKE);
+
+        //move to launch position
+        robot.autoMoveTo(-15.5, 19, 132, 8);
+        // launch preloads
+        robot.aimAtGoal();
+        autonomousLaunching(motifId);
+
+        //move off the line
+        robot.autoMoveTo(-50, 18, 90, 4);
+        robot.chill(true,0.2);
+    }
+}
