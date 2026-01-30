e2d8dbfd21eff77c0778fb50db6aafb688830777
C-Neoptolemus <cassady.fattor@gmail.com>
2026-01-29 20:41:22 -0700

    Made Blue Autos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Opmodes/Autonomous/BlueElephant.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Opmodes/Autonomous/BlueElephant.java
@@ -1,29 +1,29 @@
 package org.firstinspires.ftc.teamcode.Opmodes.Autonomous;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import org.firstinspires.ftc.teamcode.Firmware.OperatorStateMachine;
+
 import org.firstinspires.ftc.teamcode.Firmware.Systems.Spindexer;
 import org.firstinspires.ftc.teamcode.Firmware.Systems.SpindexerColorSensor;
 import org.firstinspires.ftc.teamcode.Opmodes.BaseAuto;
 
 // This Autonomous is for when the robot starts in the close zone
 @Autonomous
-public class RedElephant extends BaseAuto {
+public class BlueElephant extends BaseAuto {
     private void sortedLaunch(boolean finalLaunch, boolean firstLaunch){
         if (!firstLaunch) {
-            robot.autoMoveTo(-15.5, 30, 195, 25);
+            robot.autoMoveTo(-15.5, -30, -15, 25);
         }
 //        robot.autoMoveTo(0, 40, 135, 2);
         //The robot moves to the launch zone and it launches the three balls
         if (!finalLaunch) {
             if (firstLaunch){
-                robot.autoMoveTo(-15.5, 19, 132, 8);
+                robot.autoMoveTo(-15.5, -19, 48, 8);
             }else{
-                robot.autoMoveTo(-15.5, 19, 134, 8);
+                robot.autoMoveTo(-15.5, -19, 46, 8);
             }
 
         }else{
-            robot.autoMoveTo(-35, 18, 116, 4);
+            robot.autoMoveTo(-35, -18, 64, 4);
         }
         if (!firstLaunch) {
             robot.chill(true, 0.8);
@@ -47,7 +47,7 @@ public class RedElephant extends BaseAuto {
 
         waitForStart();
         //This is the starting location of the robot
-        robot.odometry.overridePosition(-49.82,54.5782,125.08);
+        robot.odometry.overridePosition(-49.82,-54.5782,54.92);
         robot.spindexer.setIndexOffset(Spindexer.INDEX_TYPE.NONE);
         //This is the position that the robot moves to to shoot the first three balls
         motifId = 0;
@@ -56,7 +56,7 @@ public class RedElephant extends BaseAuto {
         sortedLaunch(false, true);
 
         //move off the line
-        robot.autoMoveTo(-50, 18, 90, 4);
+        robot.autoMoveTo(-50, -18, 90, 4);
         robot.chill(true,0.2);
     }
 }
