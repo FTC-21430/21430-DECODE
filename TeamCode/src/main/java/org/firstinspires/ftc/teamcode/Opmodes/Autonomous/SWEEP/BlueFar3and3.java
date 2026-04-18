1cb04c5109d019a99b5d9a7564cf5620ecab56b1
C-Neoptolemus <cassady.fattor@gmail.com>
2026-04-18 15:13:30 -0600

    Use GlobalPositions in SWEEP autos + odometry fix
    Replace POS.* usages with GlobalPositions.POS.* and add the GlobalPositions import in BlueFar3and3 and RedFar3and3 to centralize path position constants. Also update the RedFar3and3 odometry start override to robot.odometry.overridePosition(63.5, 20.5, 0). BlueFar3and3 and RedFar3and3 spline end/start references were adjusted to use GlobalPositions.POS values.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Opmodes/Autonomous/SWEEP/BlueFar3and3.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Opmodes/Autonomous/SWEEP/BlueFar3and3.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.Opmodes.Autonomous.SWEEP;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 import org.firstinspires.ftc.teamcode.Opmodes.BaseAuto;
+import org.firstinspires.ftc.teamcode.Resources.SWEEP.GlobalPositions;
 import org.firstinspires.ftc.teamcode.Resources.SWEEP.PathPlanning;
 import org.firstinspires.ftc.teamcode.Resources.SWEEP.RobotActions;
 
@@ -17,23 +18,23 @@ public class BlueFar3and3 extends BaseAuto {
     /// path.chill(x,y,angle,duration) Wait at a specified position with a given time in seconds
     private void defineRoute(){
         PathPlanning path = robot.SWEEP.pathPlanner;
-        path.splineStart(POS.FAR_START);
+        path.splineStart(GlobalPositions.POS.FAR_START);
         path.addAction(RobotActions.Actions.PREPPING);
         path.addAction(RobotActions.Actions.TOGGLE_GOAL_AIMING);
         path.chill(1);
         path.addAction(RobotActions.Actions.LAUNCH);
         path.addAction(RobotActions.Actions.TOGGLE_GOAL_AIMING);
-        path.splineToConstantAngle(POS.INTAKE_START_CORNER);
-        path.splineToConstantAngle(POS.INTAKE_END_CORNER);
+        path.splineToConstantAngle(GlobalPositions.POS.INTAKE_START_CORNER);
+        path.splineToConstantAngle(GlobalPositions.POS.INTAKE_END_CORNER);
         path.chill(0.5);
         path.addAction(RobotActions.Actions.INTAKE);
         path.addAction(RobotActions.Actions.PREPPING);
-        path.splineToConstantAngle(POS.FAR_3);
+        path.splineToConstantAngle(GlobalPositions.POS.FAR_3);
         path.chill(1);
         path.addAction(RobotActions.Actions.TOGGLE_GOAL_AIMING);
         path.addAction(RobotActions.Actions.LAUNCH);
         path.addAction(RobotActions.Actions.TOGGLE_GOAL_AIMING);
-        path.splineEnd(POS.FAR_END);
+        path.splineEnd(GlobalPositions.POS.FAR_START);
     }
 
 
